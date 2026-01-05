6c80cae76f37495a621c69a414e8c6d67c7d804b
MatanBelfer <belfer.matan@gmail.com>
2026-01-05 09:59:49 +0200

    updated controller to work with the new input system

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -4,16 +4,17 @@ using UnityEngine.InputSystem;
 
 public class controller : MonoBehaviour
 {
-    
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private InputActionReference shootAction;
-    
+    [SerializeField] private InputActionReference aimAction;
+
+
     [SerializeField] private float speed = 30f;
-    [SerializeField] private InputActionReference MoveAction;
+    [SerializeField] private InputActionReference moveAction;
     private Rigidbody2D _rb;
     private Vector2 move;
-    
-    
+
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -23,69 +24,67 @@ public class controller : MonoBehaviour
     private void FixedUpdate()
     {
         HandleMovement();
-        // HandleShooting();
     }
-    
+
 
     private void HandleShooting()
     {
-        // Spawn Bullet
-        Bullet bullet = Instantiate(bulletPrefab,transform.position,Quaternion.identity).GetComponent<Bullet>();
-        // Set Bullet Direction & Rotation
-        // TODO : 
-        // FIGURE OUT ERROR WITH MOUSE POSITION ( ALWAYS RETURNING 0,0 )
-        Vector2 mouseScreenPosition = Mouse.current.position.ReadValue();
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, 0));
-        bullet.direction = (mouseWorldPosition - transform.position).normalized;
-        bullet.transform.rotation = Quaternion.LookRotation(bullet.direction);
-        // Set Bullet Tags
+        Bullet bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<Bullet>();
+        Vector2 mouseScreenPosition = aimAction.action.ReadValue<Vector2>();
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, Mathf.Abs(Camera.main.transform.position.z)));
+ 
+        bullet.direction = (mouseWorldPosition - transform.position);
+        bullet.direction.z = 0;
+        bullet.direction.Normalize();
+
+        float angle = Mathf.Atan2(bullet.direction.y, bullet.direction.x) * Mathf.Rad2Deg;
+        bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
         bullet.targetTag = GameManager.EnemyTag;
         bullet.ignoreTag = GameManager.PlayerTag;
-        
     }
 
     private void HandleMovement()
     {
         _rb.MovePosition(_rb.position + move * (Time.fixedDeltaTime * speed));
     }
-    
+
 
     #region Input Handle
-    
+
     private void OnEnable()
     {
         shootAction.action.Enable();
         shootAction.action.performed += OnShoot;
-        
-        MoveAction.action.Enable();
-        MoveAction.action.performed += OnMove;
-        MoveAction.action.canceled += OnMove;
+        aimAction.action.Enable();
+
+
+        moveAction.action.Enable();
+        moveAction.action.performed += OnMove;
+        moveAction.action.canceled += OnMove;
     }
-    
+
     private void OnDisable()
     {
         shootAction.action.Disable();
         shootAction.action.performed -= OnShoot;
-        
-        MoveAction.action.Disable();
-        MoveAction.action.performed -= OnMove;
-        MoveAction.action.canceled -= OnMove;
+        aimAction.action.Disable();
+
+        moveAction.action.Disable();
+        moveAction.action.performed -= OnMove;
+        moveAction.action.canceled -= OnMove;
     }
 
 
     private void OnShoot(InputAction.CallbackContext context)
     {
-        // TODO :
-        // FIX INPUT ERROR ( NOT READING MOUSE CLICK )
-        if (context.performed) {
-            HandleShooting();
-        }
+        HandleShooting();
     }
-    
+
 
     private void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
     }
+
     #endregion
 }
